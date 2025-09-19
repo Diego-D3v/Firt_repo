bc404c06afe882018f35bd6254417fd529219e3c
Diego <dadmdister@gmail.com>
2025-09-18 19:45:44 -0500

    segundo  commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require(`express`);
 const app = express();
-app.get(`/`, (req, res) => res.send(`Hola mundo desde el servidor`));
+app.get(`/`, (req, res) => res.send(`Hola mundo`));
 app.listen(3258);
